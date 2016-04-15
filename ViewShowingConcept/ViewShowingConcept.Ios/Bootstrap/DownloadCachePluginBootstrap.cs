5a4546e573936becf7fa9d76842eab806a461297
Paveshen Padayachee <istimadan@gmail.com>
2016-04-15 10:47:26 +0200

    Retarget for Ipad and IPhone (Universal) and removed unnecessary packages

--- ViewShowingConcept/ViewShowingConcept.Ios/Bootstrap/DownloadCachePluginBootstrap.cs
+++
@@ -1,9 +0,0 @@
-using MvvmCross.Platform.Plugins;
-
-namespace ViewShowingConcept.Ios.Bootstrap
-{
-    public class DownloadCachePluginBootstrap
-        : MvxLoaderPluginBootstrapAction<MvvmCross.Plugins.DownloadCache.PluginLoader, MvvmCross.Plugins.DownloadCache.iOS.Plugin>
-    {
-    }
-}
