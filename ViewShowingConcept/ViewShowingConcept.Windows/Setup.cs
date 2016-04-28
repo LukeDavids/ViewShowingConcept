7607558fc8b5dff4bc1448a7a27ed7dd82623c76
Paveshen Padayachee <istimadan@gmail.com>
2016-04-28 17:56:44 +0200

    A first go at Windows

---
+++ ViewShowingConcept/ViewShowingConcept.Windows/Setup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+using MvvmCross.WindowsUWP.Platform;
+using ViewShowingConcept.Core.Enums;
+
+namespace ViewShowingConcept.Windows
+{
+    public class Setup : MvxWindowsSetup
+    {
+
+        public Setup(Frame rootFrame) : base(rootFrame)
+        {
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+            return new Core.App();
+        }
+    }
+}
