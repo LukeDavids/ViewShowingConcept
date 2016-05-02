28fd02aad9bd7bbbbc0ddcee5d584cd2eee7c456
Paveshen Padayachee <istimadan@gmail.com>
2016-05-02 16:13:50 +0200

    less fail. Xaml does not like generics

---
+++ ViewShowingConcept/ViewShowingConcept.Windows/Interfaces/IWindowsView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.WindowsUWP.Views;
+using ViewShowingConcept.Core.Enums;
+using ViewShowingConcept.Core.ViewModels.Base;
+
+namespace ViewShowingConcept.Windows.Interfaces
+{
+    public interface IWindowsView
+    {
+        BaseViewModel BaseViewModel { get; }
+        IMvxWindowsView Page { get; }
+        ViewType ViewType { get; }
+        string ViewTag { get; set; }
+    }
+}
--- ViewShowingConcept/ViewShowingConcept.Windows/Views/Container/Container.xaml.cs
+++ ViewShowingConcept/ViewShowingConcept.Windows/Views/Container/Container.xaml.cs
@@ -7,19 +7,33 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 using MvvmCross.WindowsUWP.Views;
+using ViewShowingConcept.Core.Enums;
+using ViewShowingConcept.Core.Models;
 using ViewShowingConcept.Core.ViewModels.Container;
+using ViewShowingConcept.Windows.Interfaces;
 
 
 namespace ViewShowingConcept.Windows.Views.Container
 {
-    public sealed partial class ContainerView //: MvxWindowsPage
+    public sealed partial class ContainerView : MvxWindowsPage
     {
-        public ContainerView()
+
+        public Dictionary<ViewType, IWindowsView> Views { get; } = new Dictionary<ViewType, IWindowsView>();
+
+        private ShowViewEvent _showViewEvent;
+        public ShowViewEvent ShowViewEvent
         {
-            //this.InitializeComponent();
-            
+            get { return _showViewEvent; }
+            set
+            {
+                _showViewEvent = value;
+            }
         }
 
-       
+        public ContainerView()
+        {
+            this.InitializeComponent();
+        }
+
     }
 }
