74e6ad4997cee5c3b8eb9880d69978d01a31ccd7
Paveshen Padayachee <istimadan@gmail.com>
2016-05-06 17:13:43 +0200

    Added Viewmodel navigation test project. Currently all fail. Red light

---
+++ ViewShowingConcept/ViewShowingConcept.Core.IntegrationTests/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.Platform.Platform;
+using MvvmCross.Test.Core;
+using NUnit.Framework;
+using MvvmCross.Core;
+using MvvmCross;
+using ViewShowingConcept.Core.ViewModels;
+
+namespace ViewShowingConcept.Core.IntegrationTests
+{
+    [TestFixture]
+    public class Class1 : MvxTest
+    {
+        [Test]
+        public void ShowTabsCausesNavigation()
+        {
+            ClearAll();
+
+            var mockNavigation = CreateMockNavigation();
+            var viewModel = new CustomerDetailViewModel();
+            viewModel.ShowTabbedCommand.Execute();
+            Assert.AreEqual(1,mockNavigation.NavigateRequests.Count);
+        }
+    }
+}
---
+++ ViewShowingConcept/ViewShowingConcept.Core.IntegrationTests/MvxTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.Core.Views;
+using MvvmCross.Platform.Core;
+using MvvmCross.Test.Core;
+using ViewShowingConcept.Core.IntegrationTests.Mocks;
+
+namespace ViewShowingConcept.Core.IntegrationTests
+{
+    public class MvxTest : MvxIoCSupportingTest
+    {
+        protected MockMvxViewDispatcher CreateMockNavigation()
+        {
+            var dispatcher = new MockMvxViewDispatcher();
+            Ioc.RegisterSingleton<IMvxMainThreadDispatcher>(dispatcher);
+            Ioc.RegisterSingleton<IMvxViewDispatcher>(dispatcher);
+            return dispatcher;
+        }
+    }
+}
